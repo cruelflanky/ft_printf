7c826630d072484a450ae5bbf3f247320e7d3e12
Gravity Audry <gaudry@at-j3.21-school.ru>
2020-03-04 15:28:11 +0300

    gfgd

--- main.c
+++ main.c
@@ -1,14 +1,38 @@
 #include "ft_printf.h"
-#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strncpy_mod(char *src, int i)
+{
+	int x;
+	char *dest;
+	x = 0;
+
+	dest = (char *)ft_memalloc(sizeof(char) * i);
+	while (x < i)
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
 
 void	ft_printf(char *str, void *var, ...)
 {
+	int i;
 
+	i = 0;
+	while (str[i] != '%')
+		i++;
+	if (str[i + 1] == 's')
+		ft_putstr(ft_strjoin(ft_strncpy_mod(str, i), (char *)var));
 }
 
 int		main(void)
 {
 	char	*str = "string";
 	ft_printf("ne_moy %s", str);
+	ft_putstr("\n");
 	printf("ne_moy %s", str);
+	return (0);
 }
